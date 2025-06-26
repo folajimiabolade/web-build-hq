03e2e262aa3daa99311150179e01add2e6ef137e
Folajimi Abolade <folajimiabolade@gmail.com>
2025-06-26 18:45:58 +0100

    user to users for flask explanation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # Import necessary libraries and modules
-# flask creates the server that communicates with the user
+# flask creates the server that communicates with the users
 # https://flask.palletsprojects.com/en/stable/quickstart/
 from flask import Flask, render_template, redirect, url_for, request, flash
 # os is where the secrets are saved, like developer passwords and api keys
