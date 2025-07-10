0e71653dda3408f5c1e274661a578534f6e7df66
Folajimi Abolade <folajimiabolade@gmail.com>
2025-07-10 07:06:12 +0100

    circularize icon

--- main.py
+++ main.py
@@ -396,5 +396,10 @@ def delete_picture():
 #     return render_template("verify-email.html", form=verify_form)
 
 
+# @app.route("/api/active")
+# def active():
+#     return jsonify({"status": "active"})
+
+
 if __name__ == "__main__":
     app.run(debug=True)
